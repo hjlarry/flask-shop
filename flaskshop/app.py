306aa09e913cf2da5d4974c1999105f4d5b4aad0
he jiale <hjlarry@163.com>
2018-06-24 00:16:19 +0800

    add fake data

--- flaskshop/app.py
+++ flaskshop/app.py
@@ -72,7 +72,7 @@ def register_shellcontext(app):
 
     def shell_context():
         """Shell context objects."""
-        return {"db": db, "User": user.models.User}
+        return {"db": db, "User": user.models.User, "Product": product.models.Product}
 
     app.shell_context_processor(shell_context)
 
--- flaskshop/utils.py
+++ flaskshop/utils.py
@@ -9,3 +9,4 @@ def flash_errors(form, category="warning"):
         for error in errors:
             flash("{0} - {1}".format(getattr(form, field).label.text, error), category)
 
+
