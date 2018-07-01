132263663bdc07307213b2826bac3dc1dd2cba46
he jiale <hjlarry@163.com>
2018-07-01 14:34:57 +0800

    make login better

--- flaskshop/public/views.py
+++ flaskshop/public/views.py
@@ -28,15 +28,13 @@ def home():
 def login():
     """login page."""
     form = LoginForm(request.form)
-    # Handle logging in
-    if request.method == "POST":
-        if form.validate_on_submit():
-            login_user(form.user)
-            flash("You are logged in.", "success")
-            redirect_url = request.args.get("next") or url_for("user.members")
-            return redirect(redirect_url)
-        else:
-            flash_errors(form)
+    if form.validate_on_submit():
+        login_user(form.user)
+        flash("You are logged in.", "success")
+        redirect_url = request.args.get("next") or url_for("user.members")
+        return redirect(redirect_url)
+    else:
+        flash_errors(form)
     return render_template("public/login.html", form=form)
 
 
