b1a85860cd9ce5ba3a1528e99933245436019f17
hjlarry <hjlarry@163.com>
2018-07-05 19:24:32 +0800

    fix list style

--- flaskshop/admin/views.py
+++ flaskshop/admin/views.py
@@ -4,6 +4,7 @@ from flask import Blueprint
 from flaskshop.extensions import admin_manager, db
 from flaskshop.product.models import Product, ProductSku
 from flaskshop.order.models import Order
+from flaskshop.user.models import UserAddress
 
 blueprint = Blueprint('admin_pannel', __name__, url_prefix='/admin', static_folder='../static')
 
@@ -14,10 +15,18 @@ class ProductView(ModelView):
     edit_template = 'edit.html'
     column_list = ('id', 'title', 'image', 'on_sale', 'rating', 'sold_count', 'review_count', 'price')
     # column_filters = ('id', 'title') //TODO
-    can_delete = False
+    can_delete = True
     can_export = True
-    # can_set_page_size = True //TODO
-    inline_models = (ProductSku,)
+    can_set_page_size = True
+    # inline_models = (ProductSku,)
+
+
+class UserAddressView(ModelView):
+    list_template = 'list.html'
+    create_template = 'create.html'
+    edit_template = 'edit.html'
+    can_export = True
+    can_delete = False
 
 
 admin_manager.add_view(
@@ -25,3 +34,5 @@ admin_manager.add_view(
                 menu_icon_value='fa-bandcamp nav-icon'))
 admin_manager.add_view(ModelView(Order, db.session, endpoint='Order_admin', menu_icon_type='fa',
                                  menu_icon_value='fa-cart-arrow-down nav-icon'))
+admin_manager.add_view(UserAddressView(UserAddress, db.session, endpoint='User_address_admin', menu_icon_type='fa',
+                                       menu_icon_value='fa-cart-arrow-down nav-icon'))
