43f280ea8d6ebd3d5677e33ed0869b4cc72e06a7
hjlarry <hjlarry@163.com>
2018-08-03 11:34:09 +0800

    add variant api

--- flaskshop/api/product.py
+++ flaskshop/api/product.py
@@ -4,19 +4,33 @@ from flaskshop.product.models import Product
 
 api = Namespace('products', description='Products related operations')
 
-product = api.model('ProductList', {
+product_list = api.model('ProductList', {
     'id': fields.String(required=True, description='The product identifier'),
     'title': fields.String(required=True, description='The product name'),
     'description': fields.String(description='The product description'),
+    'price': fields.String(description='The product price'),
     'first_img': fields.String(description='The product first img', attribute='get_first_img'),
-    'images': fields.List(fields.String, description='The product images')
+})
+variant = api.model('Variant', {
+    'id': fields.String(required=True, description='The variant identifier'),
+    'sku': fields.String(required=True, description='The variant sku'),
+    'title': fields.String(required=True, description='The variant name'),
+    'price': fields.String(required=True, description='The variant price'),
+})
+product_detail = api.model('ProductDetail', {
+    'id': fields.String(required=True, description='The product identifier'),
+    'title': fields.String(required=True, description='The product name'),
+    'description': fields.String(description='The product description'),
+    'price': fields.String(description='The product price'),
+    'images': fields.List(fields.String, description='The product images'),
+    'variant': fields.List(fields.Nested(variant), description='The product variant')
 })
 
 
 @api.route('/')
 class ProductList(Resource):
     @api.doc('list_products')
-    @api.marshal_list_with(product)
+    @api.marshal_list_with(product_list)
     def get(self):
         """List all products"""
         page = request.args.get('page', 1, int)
@@ -29,7 +43,7 @@ class ProductList(Resource):
 @api.response(404, 'Product not found')
 class ProductDetail(Resource):
     @api.doc('get_product')
-    @api.marshal_with(product)
+    @api.marshal_with(product_detail)
     def get(self, id):
         """Fetch a product """
         product = Product.query.get(id)
