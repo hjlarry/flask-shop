dbc254e1c69c691991b8f1f8d018e06a4659bbc2
he jiale <hjlarry@163.com>
2018-08-05 14:04:32 +0800

    fix token

--- flaskshop/api/auth.py
+++ flaskshop/api/auth.py
@@ -1,10 +1,22 @@
-from flask import g
+from flask import g, current_app
 from flask.sessions import SecureCookieSessionInterface
 from flask_login import user_loaded_from_header
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 from flaskshop.extensions import login_manager
 from flaskshop.account.models import User
 
+serializer = Serializer(current_app.config["SECRET_KEY"], expires_in=36000)
+def generate_token(user_id):
+    return serializer.dumps({"user_id": user_id})
+
+
+def verify_token(token):
+    try:
+        data = serializer.loads(token)
+    except:
+        return False
+    return data["user_id"]
 
 class CustomSessionInterface(SecureCookieSessionInterface):
     """Prevent creating session from API requests."""
@@ -22,16 +34,14 @@ def user_loaded_from_header(self, user=None):
 
 @login_manager.request_loader
 def load_user_from_request(request):
-
     token = request.headers.get("Authorization")
-    if token:
-        token = token.replace("Basic ", "", 1)
-        # try:
-        #     api_key = base64.b64decode(api_key)
-        # except TypeError:
-        #     pass
-        user = User.query.filter_by(id=1).first()
+    try:
+        user_id = verify_token(token)
+        user = User.query.filter_by(id=user_id).first()
         if user:
             return user
-
     return None
+
+
+
+
