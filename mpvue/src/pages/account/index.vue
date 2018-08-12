45ea295bbd2a08444e9a8390016bf114cfdb52d3
he jiale <hjlarry@163.com>
2018-08-12 21:48:06 +0800

    add animate.css

--- mpvue/src/pages/account/index.vue
+++ mpvue/src/pages/account/index.vue
@@ -1,13 +1,39 @@
 <template>
     <div class="page">
+        
+        <div class="animated infinite bounce delay-2s" style="background:red;height:100rpx;width:100rpx"></div>
+        <h1 class="animated" :class="testcss">Example</h1>
+        <button class="" @click="test1">test</button>
+
+        
 
     </div>
 </template>
 
 <script>
-    export default {}
+export default {
+  data: {
+    animationData: {},
+    test:false
+  },
+  computed:{
+      testcss(){
+          if (this.test){
+              return 'fadeInUp'
+          }else{
+              return 'fadeOutDown'
+          }
+            
+      }
+  },
+  methods:{
+      test1(){
+         this.test = !this.test
+      }
+  }
+  
+};
 </script>
 
 <style>
-
 </style>
