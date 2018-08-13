52469c6edb3812c1af725c1efc617e48d84cad6a
hjlarry <hjlarry@163.com>
2018-08-13 14:33:44 +0800

    add account page

--- mpvue/src/pages/account/index.vue
+++ mpvue/src/pages/account/index.vue
@@ -1,39 +1,61 @@
 <template>
     <div class="page">
-        
-        <div class="animated infinite bounce delay-2s" style="background:red;height:100rpx;width:100rpx"></div>
-        <h1 class="animated" :class="testcss">Example</h1>
-        <button class="" @click="test1">test</button>
+        <div class="page_head">
+            <div class="userinfo-avatar">
+                <open-data type="userAvatarUrl"></open-data>
+            </div>
 
-        
+            <div class="userinfo-nickname">
+                <open-data type="userNickName"></open-data>
+            </div>
+        </div>
+
+        <div class="weui-panel">
+            <div class="weui-panel__bd">
+                <div class="weui-cells weui-cells_after-title">
+                    <div class="weui-cell">
+                        <div class="weui-cell__hd">
+                            <image src="../../static/images/order.png"
+                                   style="margin-right: 5px;vertical-align: middle;width:20px; height: 20px;"></image>
+                        </div>
+                        <div class="weui-cell__bd">My Order</div>
+                        <div class="weui-cell__ft"> > </div>
+                    </div>
+                    <div class="weui-cell">
+                        <div class="weui-cell__hd">
+                            <image src="../../static/images/address.png"
+                                   style="margin-right: 5px;vertical-align: middle;width:20px; height: 20px;"></image>
+                        </div>
+                        <div class="weui-cell__bd">My Address</div>
+                        <div class="weui-cell__ft"> > </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
     </div>
 </template>
 
 <script>
-export default {
-  data: {
-    animationData: {},
-    test:false
-  },
-  computed:{
-      testcss(){
-          if (this.test){
-              return 'fadeInUp'
-          }else{
-              return 'fadeOutDown'
-          }
-            
-      }
-  },
-  methods:{
-      test1(){
-         this.test = !this.test
-      }
-  }
-  
-};
+    export default {
+
+    };
 </script>
 
 <style>
+    .page_head {
+        display: flex;
+        height: 30%;
+        background-color: #1afa29;
+        align-items: center;
+    }
+
+    .userinfo-avatar {
+        clip-path: circle(30% at center);
+    }
+
+    .userinfo-nickname {
+        color: #000000;
+    }
 </style>
--- mpvue/src/pages/account/main.js
+++ mpvue/src/pages/account/main.js
@@ -6,6 +6,6 @@ app.$mount()
 
 export default {
   config: {
-    navigationBarTitleText: 'My Account'
+    navigationBarTitleText: 'Me'
   }
 }
