a2e500cf4d53af7179b8b314948356e45605717f
he jiale <hjlarry@163.com>
2019-01-27 17:59:53 +0800

    add admin required

--- flaskshop/dashboard/views.py
+++ flaskshop/dashboard/views.py
@@ -1,6 +1,26 @@
-from flask import Blueprint, render_template
+from functools import wraps
+from flask import Blueprint, render_template, abort
+from flask_login import login_required, current_user
 
 blueprint = Blueprint("dashboard", __name__, url_prefix="/dashboard")
+
+
+def admin_required(func):
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if not current_user.is_admin:
+            abort(401)
+        return func(*args, **kwargs)
+
+    return decorated_view
+
+
+@blueprint.before_request
+@admin_required
+@login_required
+def before_request():
+    """The whole blueprint need to login first"""
+    pass
 
 
 @blueprint.route("/")
