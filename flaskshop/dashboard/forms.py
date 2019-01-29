094d7d586d31f8910ea89f4e3dd076945620ad7a
he jiale <hjlarry@163.com>
2019-01-29 16:45:46 +0800

    add select2 plugin

--- flaskshop/dashboard/forms.py
+++ flaskshop/dashboard/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 from .models import DashboardMenu
 
@@ -9,4 +9,5 @@ class DashboardMenuForm(FlaskForm):
     order = IntegerField(default=0)
     endpoint = StringField()
     icon_cls = StringField()
+    parent_id = SelectField("Parent")
     submit = SubmitField()
