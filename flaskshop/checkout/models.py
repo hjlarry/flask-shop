31afe5c34491c5f6ffa645305df86baf13d297f4
hjlarry <hjlarry@163.com>
2019-02-22 14:47:44 +0800

    fix voucher model

--- flaskshop/checkout/models.py
+++ flaskshop/checkout/models.py
@@ -1,7 +1,3 @@
-import string
-import random
-import datetime
-
 from flask import flash
 from sqlalchemy.dialects.mysql import BOOLEAN
 from flask_login import current_user
@@ -133,54 +129,3 @@ class ShippingMethod(Model):
     def __str__(self):
         return self.title + "   $" + str(self.price)
 
-
-class CouponCode(Model):
-    __tablename__ = "coupon_codes"
-    title = Column(db.String(255), nullable=False)
-    code = Column(db.String(255), unique=True, nullable=False)
-    type = Column(db.String(255), nullable=False)
-    value = Column(db.String(255), nullable=False)
-    total = Column(db.Integer())
-    used = Column(db.Integer(), default=0)
-    min_amount = Column(db.DECIMAL(10, 2))
-    not_before = Column(db.DateTime())
-    not_after = Column(db.DateTime())
-    enabled = Column(BOOLEAN(), default=True)
-
-    @property
-    def description(self):
-        full = ""
-        if self.min_amount > 0:
-            full = "满" + str(self.min_amount)
-        if self.type == DiscountValueTypeKinds.fixed.value:
-            return full + "减" + str(self.value)
-        return full + "优惠" + str(self.value).replace(".00", "") + "%"
-
-    @classmethod
-    def generate_code(cls):
-        code = "".join(random.choices(string.ascii_uppercase, k=16))
-        exist = cls.query.filter_by(code=code).first()
-        if not exist:
-            return code
-        else:
-            return cls.generate_code()
-
-    def check_available(self, order_total_amount=None):
-        if not self.enabled:
-            raise Exception("This code can not use by system")
-        if self.total - self.used < 0:
-            raise Exception("The coupon has been redeemed")
-        if self.not_before and self.not_before > datetime.datetime.now():
-            raise Exception("The coupon can not use now, please retry later")
-        if self.not_after and self.not_after < datetime.datetime.now():
-            raise Exception("The coupon has expired")
-        if order_total_amount and order_total_amount < self.min_amount:
-            raise Exception(
-                "The order amount does not meet the minimum amount of the coupon"
-            )
-        return True
-
-    def get_adjusted_price(self, order_total_amount):
-        if self.type == DiscountValueTypeKinds.fixed.value:
-            return max(0.01, float(order_total_amount) - float(self.value))
-        return float(order_total_amount) * (100 - float(self.value)) / 100
--- flaskshop/discount/models.py
+++ flaskshop/discount/models.py
@@ -1,25 +1,47 @@
-from flaskshop.database import Column, Model, db
+import random
+import string
+import datetime
+
 from sqlalchemy.dialects.mysql import TINYINT
+
+from flaskshop.database import Column, Model, db
 
 
 class Voucher(Model):
     __tablename__ = "discount_voucher"
     type = Column(TINYINT())
     title = Column(db.String(255))
-    code = Column(db.String(12))
+    code = Column(db.String(16), unique=True)
     usage_limit = Column(db.Integer())
-    used = Column(db.Integer())
+    used = Column(db.Integer(), default=0)
     start_date = Column(db.DateTime())
     end_date = Column(db.DateTime())
     discount_value_type = Column(TINYINT())
     discount_value = Column(db.DECIMAL(10, 2))
-    apply_to = Column(db.String(20))
     limit = Column(db.DECIMAL(10, 2))
     category_id = Column(db.Integer())
     product_id = Column(db.Integer())
 
     def __str__(self):
         return self.title
+
+    @classmethod
+    def generate_code(cls):
+        code = "".join(random.choices(string.ascii_uppercase, k=16))
+        exist = cls.query.filter_by(code=code).first()
+        if not exist:
+            return code
+        else:
+            return cls.generate_code()
+
+    def check_available(self, order_total_amount=None):
+        if self.start_date and self.start_date > datetime.datetime.now():
+            raise Exception("The voucher code can not use now, please retry later")
+        if self.end_date and self.end_date < datetime.datetime.now():
+            raise Exception("The voucher code has expired")
+        if self.usage_limit and self.usage_limit - self.used < 0:
+            raise Exception("This voucher code has been used out")
+        return True
 
 
 class Sale(Model):
@@ -42,3 +64,4 @@ class SaleProduct(Model):
     __tablename__ = "discount_sale_products"
     sale_id = Column(db.Integer())
     product_id = Column(db.Integer())
+
