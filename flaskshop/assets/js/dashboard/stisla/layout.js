6513dddc462d64698994f7938f459006fc8c4b66
hjlarry <hjlarry@163.com>
2019-03-07 14:27:21 +0800

    merge stisla js file

---
+++ flaskshop/assets/js/dashboard/stisla/layout.js
@@ -0,0 +1,23 @@
+// General JS Scripts
+import 'bootstrap';
+import 'popper.js';
+import 'tooltip.js';
+import 'jquery.nicescroll';
+import 'moment';
+import './stisla';
+
+// JS Libraies
+import 'jquery-sparkline';
+import 'chart.js';
+import 'owl.carousel2';
+import 'summernote';
+import 'chocolat';
+
+// Page Specific JS File
+import './page/index';
+
+
+// Template JS File
+import './scripts';
+import './custom';
+
