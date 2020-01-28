71524f0b7777caa51ccc43ce6e03efb098502079
he jiale <hjlarry@163.com>
2020-01-28 21:44:55 +0800

    fix repeat create payment for same order bug

--- flaskshop/order/views.py
+++ flaskshop/order/views.py
@@ -35,27 +35,39 @@ def show(token):
     return render_template("orders/details.html", order=order)
 
 
-# def create_payment(token, payment_method):
-    
-
-@blueprint.route("/pay/<string:token>/alipay")
-@login_required
-def ali_pay(token):
+def create_payment(token, payment_method):
     order = Order.query.filter_by(token=token).first()
     if order.status != OrderStatusKinds.unfulfilled.value:
         abort(403, "This Order Can Not Pay")
     payment_no = str(int(time.time())) + str(current_user.id)
-    order_string = zhifubao.send_order(order.token, payment_no, order.total)
     customer_ip_address = request.headers.get("X-Forwarded-For", request.remote_addr)
-    OrderPayment.create(
-        order_id=order.id,
-        payment_method="alipay",
-        payment_no=payment_no,
-        status=PaymentStatusKinds.waiting.value,
-        total=order.total,
-        customer_ip_address=customer_ip_address,
-    )
-    return redirect(current_app.config["PURCHASE_URI"] + order_string)
+    payment = OrderPayment.query.filter_by(order_id=order.id).first()
+    if payment:
+        payment.update(
+            payment_method=payment_method,
+            payment_no=payment_no,
+            customer_ip_address=customer_ip_address,
+        )
+    else:
+        payment = OrderPayment.create(
+            order_id=order.id,
+            payment_method=payment_method,
+            payment_no=payment_no,
+            status=PaymentStatusKinds.waiting.value,
+            total=order.total,
+            customer_ip_address=customer_ip_address,
+        )
+    if payment_method == "alipay":
+        order_string = zhifubao.send_order(order.token, payment_no, order.total)
+        payment.order_string = order_string
+    return payment
+
+
+@blueprint.route("/pay/<string:token>/alipay")
+@login_required
+def ali_pay(token):
+    payment = create_payment(token, "alipay")
+    return redirect(current_app.config["PURCHASE_URI"] + payment.order_string)
 
 
 @blueprint.route("/alipay/notify", methods=["POST"])
@@ -76,34 +88,16 @@ def ali_notify():
 @blueprint.route("/pay/<string:token>/testpay")
 @login_required
 def test_pay(token):
-    order = Order.query.filter_by(token=token).first()
-    if order.status != OrderStatusKinds.unfulfilled.value:
-        abort(403, "This Order Can Not Pay")
-    payment_no = str(int(time.time())) + str(current_user.id)
-    customer_ip_address = request.headers.get("X-Forwarded-For", request.remote_addr)
-    payment = OrderPayment.query.filter_by(order_id=order.id).first()
-    if payment:
-        payment.update(
-            payment_method="testpay",
-            payment_no=payment_no,
-            customer_ip_address=customer_ip_address,
-        )
-    else:
-        payment = OrderPayment.create(
-            order_id=order.id,
-            payment_method="testpay",
-            payment_no=payment_no,
-            status=PaymentStatusKinds.waiting.value,
-            total=order.total,
-            customer_ip_address=customer_ip_address,
-        )
+    payment = create_payment(token, "testpay")
     payment.pay_success(paid_at=datetime.now())
     return redirect(url_for("order.payment_success"))
+
 
 @blueprint.route("/payment_success")
 @login_required
 def payment_success():
     return render_template("orders/checkout_success.html")
+
 
 @blueprint.route("/cancel/<string:token>")
 @login_required
