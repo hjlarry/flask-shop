b0944e40ed7bd425ac302da14f55a8436f8ef92e
hjlarry <hjlarry@163.com>
2020-03-02 16:32:24 +0800

    order list in dashboard support query condion

--- flaskshop/assets/js/dashboard/voucher.js
+++ flaskshop/assets/js/dashboard/voucher.js
@@ -2,4 +2,17 @@ import 'daterangepicker';
 import './select';
 
 
-$('#reservation').daterangepicker();
+$('#reservation').daterangepicker({
+    autoUpdateInput: false,
+      locale: {
+          cancelLabel: 'Clear'
+      }
+});
+
+$('#reservation').on('apply.daterangepicker', function(ev, picker) {
+    $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+});
+
+$('#reservation').on('cancel.daterangepicker', function(ev, picker) {
+    $(this).val('');
+});
--- flaskshop/dashboard/views/order.py
+++ flaskshop/dashboard/views/order.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import request, render_template
 
 from flaskshop.order.models import Order
@@ -6,7 +8,21 @@ from flaskshop.constant import OrderStatusKinds, ShipStatusKinds
 
 def orders():
     page = request.args.get("page", type=int, default=1)
-    pagination = Order.query.order_by(Order.id.desc()).paginate(page, 10)
+    query = Order.query.order_by(Order.id.desc())
+
+    status = request.args.get("status", type=int)
+    if status:
+        query = query.filter_by(status=status)
+    order_no = request.args.get("order_number", type=str)
+    if order_no:
+        query = query.filter(Order.token.like(f"%{order_no}%"))
+    created_at = request.args.get("created_at", type=str)
+    if created_at:
+        start_date, end_date = created_at.split("-")
+        start_date = datetime.strptime(start_date.strip(), "%m/%d/%Y")
+        end_date = datetime.strptime(end_date.strip(), "%m/%d/%Y")
+        query = query.filter(Order.created_at.between(start_date, end_date))
+    pagination = query.paginate(page, 10)
     props = {
         "id": "ID",
         "identity": "Identity",
@@ -16,10 +32,10 @@ def orders():
         "created_at": "Created At",
     }
     context = {
-        "title": "Order List",
         "items": pagination.items,
         "props": props,
         "pagination": pagination,
+        "order_stats_kinds": OrderStatusKinds,
     }
     return render_template("order/list.html", **context)
 
