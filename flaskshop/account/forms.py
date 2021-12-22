74e478c329d769c122d7ff24ab43e51735081a70
dedal <grigor.kolev@gmail.com>
2021-12-22 14:27:52 +0200

    Reset user password

--- flaskshop/account/forms.py
+++ flaskshop/account/forms.py
@@ -54,6 +54,33 @@ class RegisterForm(FlaskForm):
             return False
         return True
 
+class ResetPasswd(FlaskForm):
+    '''Password reset'''
+    username = StringField(lazy_gettext("Email"), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(ResetPasswd, self).__init__(*args, **kwargs)
+        self.user = None
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(ResetPasswd, self).validate()
+        if not initial_validation:
+            return False
+
+        if "@" in self.username.data:
+            self.user = User.query.filter_by(email=self.username.data).first()
+            if not self.user:
+                self.username.errors.append(lazy_gettext("Unknown username"))
+                return False
+            if not self.user.is_active:
+                self.username.errors.append(lazy_gettext("User not activated"))
+                return False
+        else:
+            self.username.errors.append(lazy_gettext("Invalid"))
+            return False
+        return True
 
 class LoginForm(FlaskForm):
     """Login form."""
