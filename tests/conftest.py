a2bf34c8e79c4ad4471e0b7bb96dec94720119ce
hejl <hjlarry@163.com>
2022-09-15 14:54:18 +0800

    add some end to end tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """Defines fixtures available to all tests."""
 import pytest
+from pathlib import Path
 
 from flaskshop.app import create_app
 from flaskshop.database import db as _db
-from flaskshop.random_data import create_menus
+from flaskshop.random_data import create_menus, create_products_by_schema
 from flaskshop.utils import jinja_global_varibles
 
 
@@ -32,8 +33,11 @@ def db(app):
     _db.app = app
     with app.app_context():
         _db.create_all()
-        for msg in create_menus():
-            print(msg)
+
+        create_menus()
+        create_products_by_schema(
+            placeholder_dir=Path("placeholders"), how_many=1, create_images=False
+        )
 
     yield _db
 
